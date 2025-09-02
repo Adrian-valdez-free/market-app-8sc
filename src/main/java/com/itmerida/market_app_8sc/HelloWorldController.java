92ba6ce9896e8f4d575232fc0959a7051910d4fb
Jose Adrian <joseadria_v12@hotmail.com>
2025-09-02 17:22:28 -0600

    Spring Listo

---
+++ src/main/java/com/itmerida/market_app_8sc/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.itmerida.market_app_8sc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/Hola")
+public class HelloWorldController {
+
+    @GetMapping("/saludar")  
+    public String saludar(){
+        return "!Hola Mundo¡";
+    }
+}
